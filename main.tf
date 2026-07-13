d491dede7499749f0574fc094954fa7a0ff42139
Avi <avinashshetty2004@gmail.com>
2026-07-14 00:24:42 +0530

    added nat to infra

--- main.tf
+++ main.tf
@@ -1,18 +1,105 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
 
-  name = "my-ownvpc"
-  cidr = "10.0.0.0/16"
+resource "aws_subnet" "privatesub1" {
+  cidr_block = "10.0.0.0/24"
+  vpc_id = aws_vpc.main.id
+  availability_zone = "ap-south-1a"
+   tags = {
+    Name = "privatesub1"
+  }
+}
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+resource "aws_subnet" "privatesub2" {
+  cidr_block = "10.0.0.0/24"
+  vpc_id = aws_vpc.main.id
+  availability_zone = "ap-south-1b"
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+     tags = {
+    Name = "privatesub2"
+  }
+}
+
+resource "aws_subnet" "publicsub1" {
+  cidr_block = "10.0.0.0/24"
+  vpc_id = aws_vpc.main.id
+  availability_zone = "ap-south-1a"
+
+     tags = {
+    Name = "publicsub1"
+  }
+}
+
+resource "aws_subnet" "publicsub2" {
+  cidr_block = "10.0.0.0/24"
+  vpc_id = aws_vpc.main.id
+  availability_zone = "ap-south-1b"
+
+     tags = {
+    Name = "publicsub2"
+  }
+}
+
+resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_nat_gateway" "NATforprivate1" {
+  allocation_id = aws_eip.example.id
+  subnet_id     = aws_subnet.
 
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    Name = "gw NAT"
   }
+
+  depends_on = [aws_internet_gateway.myigw.id]
+}
+
+resource "aws_route_table" "RTforpublicsub" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "RTforpublicsub"
+  }
+}
+
+resource "aws_route" "name" {
+  route_table_id = aws_route_table.RTforpublicsub.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.myigw.id
+}
+
+resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "RTforpublicsub" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "RTforpublicsub"
+  }
+}
+
+resource "aws_route" "name" {
+  route_table_id = aws_route_table.RTforpublicsub.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.myigw.id
+}resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "RTforpublicsub" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "RTforpublicsub"
+  }
+}
+
+resource "aws_route" "name" {
+  route_table_id = aws_route_table.RTforpublicsub.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.myigw.id
 }
